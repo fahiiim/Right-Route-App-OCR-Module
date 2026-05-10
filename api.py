de8b6a7de4197995923afb98369cbd5101c0cc90
fahiiim <fahimsarker0805@gmail.com>
2026-05-10 16:38:57 +0600

    ip change

--- api.py
+++ api.py
@@ -184,7 +184,7 @@ async def get_supported_formats():
 
 if __name__ == "__main__":
     import uvicorn
-    host = os.getenv("HOST", "10.10.7.81")
+    host = os.getenv("HOST", "10.10.20.43")
     port = int(os.getenv("PORT", "8001"))
     uvicorn.run(app, host=host, port=port)
 
